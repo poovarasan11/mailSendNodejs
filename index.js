83cf14b26f87e2d9af463c4c3d3259489b07e99f
poovarasan <poovarasan1999skp@gmail.com>
2022-12-02 12:24:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const nodemailer = require('nodemailer'); //use nodemail npm and send to maill access on the mail 2 step verify allow the our mail
+console.log(`Wellcome To Mail Send Application `);
+const sender = nodemailer.createTransport({
+    service: 'gmail', //servies mail type
+    host: `smtp.zoho.com`, //send the zoho mail account 
+    host: "smtp-mail.outlook.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "pootesting9@gmail.com",
+        pass: `************`
+        // pass: "123Anajvnhiqanmjnqts"     //123A create the our mail 2 step verify and ganarete the default password setup the default password 
+    }
+})
+
+const composemail = {
+    from: "pootesting9@gmail.com",
+    to: "poovarasan.s@fasoftwares.com,pootesting9@gmail.com",
+    subject: 'Hii poo Testing Only ',
+    text: "Hello poo Wellcome"
+}
+sender.sendMail(composemail, function (err, info) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("mail send successfully" + info.response);
+    }
+})
+
+
